399e017212e0de3d5e5092bc70c816336ddc0527
Syed Javeed <syedjm18@gmail.com>
2023-02-10 00:11:58 +0530

    Second Commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,18 +2,19 @@ package com.driver;
 
 public class Main {
   public static class A{
-      String meth(){
+      public String meth(){
           return "Invoking method from class A";
       }
   }
   public static class B extends A{
       @Override
-      String meth(){
+      public String meth(){
           return "Method is overridden in Extendend class B";
       }
   }
 
     public static void main(String[] args) {
+
         B b = new B();
         b.meth();
 
